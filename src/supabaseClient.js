3b0d62fcac07cece3d4f27bff0611d91bbba5e60
Overland865 <113620174+Overland865@users.noreply.github.com>
2026-08-18 17:22:04 -0600

    ci: agregar github action para despliegue automatico y soporte base gh-pages

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Vite usa import.meta.env para leer el archivo .env
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || "https://qpqocbhsyyovraozukfh.supabase.co";
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || "sb_publishable_raX0JK7dhrFwhT2i6pVskw_EUmG0F0x";
 
 // Creamos y exportamos la conexión lista para usarse en cualquier parte
 export const supabase = createClient(supabaseUrl, supabaseKey);
